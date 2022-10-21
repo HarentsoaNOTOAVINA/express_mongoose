424947e20c23c1a296f375fcfd1f84d9ede1b3ff
Harentsoa NOTOAVINA <100120905+Harentsoa-NOTOAVINA@users.noreply.github.com>
2022-10-21 17:48:26 +0300

    adding project

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./app/models");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+
+// connect to db
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Conected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+// set ports, listening for requests
+const PORT = process.env.PORT || 8070;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
